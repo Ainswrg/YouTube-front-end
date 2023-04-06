41fbc19f7b78c08332fee0a65eb900d14012abc1
JaslanD <jaslan.dlnv@gmail.com>
2023-04-06 23:23:33 +0300

    feat: add some style and fonts

---
+++ app/components/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+import { FC, PropsWithChildren } from 'react'
+
+const Home: FC<PropsWithChildren> = () => {
+  return <div>Home</div>
+}
+
+export default Home
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
+      <link rel='preconnect' href='https://fonts.googleapis.com' />
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap'
+        rel='stylesheet'
+      ></link>
       <body>
         <Main />
         <NextScript />
